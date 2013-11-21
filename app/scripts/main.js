1eb2d378be6d7e9f5cde0959dad489e5b6b67862
Carter Tsai <hamming1@gmail.com>
2013-11-22 00:17:25 +0800

    Add Keypress event to move rect

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+var stage = new Kinetic.Stage({
+  container: 'game',
+  width: 600,
+  height: 400,
+});
+
+var layer = new Kinetic.Layer();
+
+var rect = new Kinetic.Rect({
+  x: 10,
+  y: 120,
+  width: 15,
+  height: 150,
+  fill: 'red',
+  stroke: 'black',
+  strokeWidth: 4
+});
+
+layer.add(rect);
+stage.add(layer);
+
+window.addEventListener('keydown', function(e) {
+    var old_x = rect.attrs.x;
+    var old_y = rect.attrs.y;
+ //   if (e.keyCode == 37) //Left Arrow Key
+//        rect.setAttrs({x:old_x-10});
+    if (e.keyCode == 38) //Up Arrow Key
+        rect.setAttrs({y:old_y-15});
+//    if (e.keyCode == 39) //Right Arrow Key
+//        rect.setAttrs({x:old_x+10});
+    if (e.keyCode == 40) //Top Arrow Key
+        rect.setAttrs({y:old_y+15});
+    layer.draw();
+});
